859c0c675fe554ca37c00e8ca0825ce6a4200e1e
Marco <marco.dusty.hofmann@gmail.com>
2025-12-05 19:11:00 +0100

    Change 1.0

--- geometry.go
+++ geometry.go
@@ -1,11 +1,16 @@
 package geometry
 import(
-"fmt"
 "math"
 )
 
 type Point struct{
 X,Y float64
+}
+
+func Media(p1,p2 Point)(P Point){
+P.x=(p1.X+p2.X)/2
+P.Y=(p1.Y+p2.Y)/2
+return
 }
 
 func CreateAPoint(x,y float64)  (P Point){
